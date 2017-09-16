0eaaaef9b59d860c5f15d636507831e8b036e2c3
Daniel Molczyk <danielmolczyk@gmail.com>
2017-09-15 22:17:07 -0400

    Added rideable path drawing

--- Assets/Scripts/DrawListener.cs
+++ Assets/Scripts/DrawListener.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 [RequireComponent(typeof(LineRenderer))]
 public class DrawListener : MonoBehaviour {
 
-	List<Vector3> linePoints = new List<Vector3>();
-	LineRenderer lineRenderer;
 	public float startWidth = 1.0f;
 	public float endWidth = 1.0f;
 	public float threshold = 0.001f;
-	Camera thisCamera;
-	int lineCount = 0;
+	public GameObject playerPaths;
+	public Material pathMaterial;
 
+	List<Vector3> pathPoints = new List<Vector3>();
+	int pathCount = 0;
 	Vector3 lastPos = Vector3.one * float.MaxValue;
+	int pathsDrawn = 0;
+
+	LineRenderer lineRenderer;
+	Camera thisCamera;
 
 	// Use this for initialization
 	void Start () {
@@ -22,31 +26,62 @@ public class DrawListener : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 mousePos = Input.mousePosition;
-		Vector3 mouseWorld = thisCamera.ScreenToWorldPoint(mousePos);
-		mouseWorld.z = 0;
+		// Get input
+		bool mouseDown = Input.GetMouseButton(0);
+		bool mouseUp = Input.GetMouseButtonUp(0);
 
-		float dist = Vector3.Distance(lastPos, mouseWorld);
-		if (dist <= threshold)
-			return;
+		// Draw path while dragging
+		if (mouseDown) {
+			Vector3 mousePos = Input.mousePosition;
+			Vector3 mouseWorld = thisCamera.ScreenToWorldPoint(mousePos);
+			mouseWorld.z = 0;
 
-		lastPos = mouseWorld;
-		if (linePoints == null)
-			linePoints = new List<Vector3>();
-		linePoints.Add(mouseWorld);
+			float dist = Vector3.Distance(lastPos, mouseWorld);
+			if (dist <= threshold)
+				return;
 
-		UpdateLine();
+			lastPos = mouseWorld;
+			if (pathPoints == null)
+				pathPoints = new List<Vector3>();
+			pathPoints.Add(mouseWorld);
+
+			UpdatePath();
+		} else if (mouseUp) {
+			CompletePath();
+			ResetPath();
+		}
 	}
 
-     void UpdateLine() {
+     void UpdatePath() {
 		lineRenderer.startWidth = startWidth;
 		lineRenderer.endWidth = endWidth;
-		lineRenderer.numPositions = linePoints.Count;
+		lineRenderer.positionCount = pathPoints.Count;
 
-		for(int i = lineCount; i < linePoints.Count; i++) {
-			lineRenderer.SetPosition(i, linePoints[i]);
+		for(int i = pathCount; i < pathPoints.Count; i++) {
+			lineRenderer.SetPosition(i, pathPoints[i]);
 		}
 
-		lineCount = linePoints.Count;
+		pathCount = pathPoints.Count;
      }
+
+	 void CompletePath() {
+		// Create new path
+		pathsDrawn++;
+		GameObject pathGO = new GameObject("Path " + pathsDrawn);
+		pathGO.transform.parent = playerPaths.transform;
+		LineRenderer lineRend = pathGO.AddComponent<LineRenderer>();
+		lineRend.material = pathMaterial;
+		lineRend.startWidth = startWidth;
+		lineRend.endWidth = endWidth;
+		lineRend.positionCount = pathPoints.Count;
+		lineRend.SetPositions(pathPoints.ToArray());
+		RidePath ridePath = pathGO.AddComponent<RidePath>();
+	 }
+
+	 void ResetPath() {
+		lastPos = Vector3.one * float.MaxValue;
+		pathPoints.Clear();
+		pathCount = 0;
+		lineRenderer.positionCount = 0;
+	 }
 }
